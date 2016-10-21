ef0f4266291e40d5d34668699c9bfb849ce0c71f
Henry Zou <zouhenry@gmail.com>
2016-10-21 11:34:06 -0400

    adding event HttpBackup_cached

--- dist/httpbackup.js
+++ dist/httpbackup.js
@@ -94,18 +94,21 @@
 	  };
 	
 	  function response(response) {
+	    var urlKey = getCacheKey(response.config);
 	    // for every successful request, cache the response
-	    httpBackupCache.setItem(getCacheKey(response.config), response);
+	    httpBackupCache.setItem(urlKey, response);
+	    $rootScope.$emit('HttpBackup_cached', { url: urlKey, response: response });
 	    return response;
 	  }
 	
 	  function responseError(response) {
-	    console.log('before getKey');
-	    var data = httpBackupCache.getItem(getCacheKey(response.config));
-	    if (data) {
+	    var urlKey = getCacheKey(response.config);
+	
+	    var cachedResponse = httpBackupCache.getItem(urlKey);
+	    if (cachedResponse) {
 	      //if response fails and there's cached data
-	      $rootScope.$emit('HttpBackup_activated', { url: response.config.url, response: data });
-	      return $q.resolve(data);
+	      $rootScope.$emit('HttpBackup_activated', { url: urlKey, response: cachedResponse });
+	      return $q.resolve(cachedResponse);
 	    } else {
 	      return $q.reject(response);
 	    }
